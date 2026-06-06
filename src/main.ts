df084f5d47be5de977b45ff1d50baa60d05aca46
Samuel Omisope <omisope34@gmail.com>
2026-06-06 21:11:30 +0100

    Add Vercel URL to CORS

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  origin: [
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://testyourself-nu.vercel.app/', // replace with your actual Vercel URL
+  ],
+  methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+});
 
   await app.listen(3000);
   console.log('🚀 Server running on http://localhost:3000');
