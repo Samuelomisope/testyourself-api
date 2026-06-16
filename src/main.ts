7a8ebfc35e4ee29dc8be602d9bcc1e2c2c5e09ad
Samuel Omisope <omisope34@gmail.com>
2026-06-16 10:00:54 +0100

    Add the express import.

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import * as express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
   app.use(helmet({
     crossOriginOpenerPolicy: false,
