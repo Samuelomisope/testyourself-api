87df9319b29d4f1ff327b7ff012dd110be1cbdc4
Samuel Omisope <omisope34@gmail.com>
2026-08-28 03:11:13 +0100

    Add admin analytics endpoint and university news feature

--- src/universities/universities.module.ts
+++ src/universities/universities.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UniversitiesService } from './universities.service';
 import { UniversitiesController } from './universities.controller';
+import { UniversityNewsController } from './university-news.controller';
+import { UniversityNewsService } from './university-news.service';
 import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [UniversitiesController],
-  providers: [UniversitiesService],
-  exports: [UniversitiesService],
+  controllers: [UniversitiesController, UniversityNewsController],
+  providers: [UniversitiesService, UniversityNewsService],
+  exports: [UniversitiesService, UniversityNewsService],
 })
 export class UniversitiesModule {}
